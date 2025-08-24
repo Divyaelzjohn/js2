7440b9a8af2f4e40753d69a7d5cdc73a2c27b57b
Divya <divelzjon@gmail.com>
2025-08-24 22:31:27 +0530

    JS56

---
+++ Final/Error-Handling/script.js
@@ -0,0 +1,43 @@
+/*try{
+  let num=10/0;
+  console.log("Result",num)
+  let result=x+5
+}
+catch(error){
+  console.log("Error occured:",error.message)
+}finally{
+  console.log("Execution finished!")
+}
+
+// throwing errors-.you can create your own error using throw.
+function divide(a, b) {
+if (b === 0) {
+  throw new Error("Cannot divide by zero!");
+}
+return a / b;
+}
+
+try {
+  console.log(divide(10, 0));
+}
+catch (error) {
+  console.log("Caught error:", error.message);
+}
+*/
+
+
+// Debugging techniques- to find errors in javascript
+/* 1.Console methods 
+    -console.log()->Print values.
+    -console.error()->print error message
+    -console.warn()-> Warnig
+    -console.table()->Display arrays/Objects in table
+  2.Breakpoints in browser DevTools
+    -Open DevTools ->Sources -> Add breakpoints ->Step through code
+  3.Debugger keyword
+    Stops code at a point(like a breakpoint)
+*/
+    // let X=5; let Y=0;debugger;console.log(x+y);
+    // Debugging errors(wrong username// password) Debugging API data fetching.
+
+
